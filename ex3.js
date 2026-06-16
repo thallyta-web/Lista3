776398e113795082428dae5b87eb76336368b072
alunocbra <alunocbra@ifb.local>
2026-06-16 17:40:24 -0300

    Adicionando ex3

---
+++ ex3.js
@@ -0,0 +1,17 @@
+let codigoProduto = 1;
+switch(codigoProduto){
+    case 1:
+        console.log("Hambúrguer");
+        break;
+    case 2:
+        console.log("Batata Frita");
+        break;
+    case 3:
+        console.log("Refrigerante");
+        break;
+    case 4:
+        console.log("Milkshake");
+        break;
+    default:
+        console.log("Produto não encontrado!")
+}
